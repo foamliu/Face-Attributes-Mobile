8152d4749f9a9b3002c931626f0f1c4a4dafcd5c
unknown <foamliu@yeah.net>
2019-12-08 09:48:34 +0800

    update

--- pre_process.py
+++ pre_process.py
@@ -36,12 +36,15 @@ if __name__ == "__main__":
 
     samples = []
     for item in tqdm(items):
-        full_path = item['full_path']
-        img = cv.imread(full_path)
-        bboxes, landmarks = detect_faces(img)
-        idx = select_significant_face(bboxes)
-        b = bboxes[idx]
-        img = img[int(b[1]):int(b[3]), int(b[0]):int(b[2])]
-        filename = full_path.replace('data/CASIA-WebFace/', '').replace('/', '_')
-        filename = os.path.join(IMG_DIR, filename)
-        cv.imwrite(filename, img)
+        try:
+            full_path = item['full_path']
+            img = cv.imread(full_path)
+            bboxes, landmarks = detect_faces(img)
+            idx = select_significant_face(bboxes)
+            b = bboxes[idx]
+            img = img[int(b[1]):int(b[3]), int(b[0]):int(b[2])]
+            filename = full_path.replace('data/CASIA-WebFace/', '').replace('/', '_')
+            filename = os.path.join(IMG_DIR, filename)
+            cv.imwrite(filename, img)
+        except Exception as err:
+            print(err)
