eba596e7771cb7cb19c537458781379a51c54986
Yang Liu <liuyang12@focusmedia.cn>
2019-12-10 13:51:47 +0800

    update

--- export.py
+++ export.py
@@ -25,3 +25,14 @@ if __name__ == '__main__':
     model = FaceAttributeModel()
     model.load_state_dict(torch.load(filename))
     print('elapsed {} sec'.format(time.time() - start))
+
+    filename_scripted = 'face-attributes_scripted.pt'
+    print('saving {}...'.format(filename_scripted))
+    start = time.time()
+    torch.jit.save(torch.jit.script(model), filename_scripted)
+    print('elapsed {} sec'.format(time.time() - start))
+
+    print('loading {}...'.format(filename))
+    start = time.time()
+    model = torch.jit.load(filename_scripted)
+    print('elapsed {} sec'.format(time.time() - start))
